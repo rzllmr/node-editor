8f1985b085caa6e3035cb463986566380e52fb82
rzllmr <30299783+rzllmr@users.noreply.github.com>
2020-01-22 00:58:09 +0100

    Hide dev tools for packaged app
    Additional changes:
     - Include linter configuration for consistent code style

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 
 const {app, BrowserWindow} = require('electron');
+const env = require('./env.js');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win;
 
 function createWindow() {
-  // Create the browser window.
   win = new BrowserWindow({
     width: 1200,
     height: 900,
@@ -17,38 +15,28 @@ function createWindow() {
   });
   win.webContents.session.clearCache(() => {});
 
-  // and load the index.html of the app.
   win.loadFile('index.html');
 
-  // Open the DevTools.
-  win.webContents.openDevTools();
+  if (env.debug) {
+    win.webContents.openDevTools();
+  }
 
-  // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null;
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+  // support macOS standard to keep application running until quit explicitly
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+  // support macOS standard to open a new window when none is present
   if (win === null) {
     createWindow();
   }
