4d09cfab4bee53bcce6f827af3053862ee8d5459
rzllmr <30299783+rzllmr@users.noreply.github.com>
2020-12-30 23:06:13 +0100

    Store Graph end types in file

--- scripts/graph.js
+++ scripts/graph.js
@@ -264,11 +264,13 @@ C${ctrl1.x},${ctrl1.y} ${ctrl2.x},${ctrl2.y} ${target.x},${target.y}`;
 
   export() {
     const element = this.element[0];
+    const source = this.anchors.source;
+    const target = this.anchors.target;
     const object = {
       board: this.boardId,
       type: element.className.baseVal.split(' ')[0],
-      source: element.id.split('-')[0],
-      target: element.id.split('-')[1]
+      source: source.id + '.' + source.end.charAt(0),
+      target: target.id + '.' + target.end.charAt(0)
     };
     return object;
   }
@@ -277,17 +279,19 @@ C${ctrl1.x},${ctrl1.y} ${ctrl2.x},${ctrl2.y} ${target.x},${target.y}`;
     const sides = {'t': 'top', 'r': 'right', 'b': 'bottom', 'l': 'left'};
 
     // create source anchor
-    const sourceMatch = object.source.match(/(\d+)(\D)(\d+)/);
-    const source = {id: sourceMatch[1], side: sides[sourceMatch[2]], index: sourceMatch[3]};
+    const sourceMatch = object.source.match(/(\d+)(\w)(\d+)\.(\w)/);
+    const source = {id: sourceMatch[1], side: sides[sourceMatch[2]], index: sourceMatch[3],
+      end: sourceMatch[4] == 's' ? 'source' : 'target'};
     const sourceNode = this.proxy.resolve(source.id);
-    const sourceAnchor = sourceNode.addAnchorDirectly('source', source.side, source.index);
+    const sourceAnchor = sourceNode.addAnchorDirectly(source.end, source.side, source.index);
     sourceAnchor.registerDragOut();
 
     // create target anchor
-    const targetMatch = object.target.match(/(\d+)(\D)(\d+)/);
-    const target = {id: targetMatch[1], side: sides[targetMatch[2]], index: targetMatch[3]};
+    const targetMatch = object.target.match(/(\d+)(\w)(\d+)\.(\w)/);
+    const target = {id: targetMatch[1], side: sides[targetMatch[2]], index: targetMatch[3],
+      end: targetMatch[4] == 's' ? 'source' : 'target'};
     const targetNode = this.proxy.resolve(target.id);
-    const targetAnchor = targetNode.addAnchorDirectly('target', target.side, target.index);
+    const targetAnchor = targetNode.addAnchorDirectly(target.end, target.side, target.index);
     targetAnchor.registerDragOut();
 
     // connect source to target anchor
